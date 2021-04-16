529ca6844f34ed24e85c45a1745519dd44593b8e
Anya Ji <aj592@cornell.edu>
2021-04-16 02:32:37 -0400

    clips pages

---
+++ pages/am2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactPlayer from "react-player";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ReactPlayer
+        className="react-player"
+        url="/video/chili2.mp4"
+        controls={false}
+        playing
+      />
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,5 @@ import ReactPlayer from "react-player";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  var [playing, setPlaying] = useState(false);
-  var handleOnReady = () => setTimeout(() => setPlaying(true), 10);
-  return (
-    <div className={styles.container}>
-      <ReactPlayer
-        onReady={handleOnReady}
-        className="react-player"
-        url="/video/prac.mp4"
-        controls={false}
-        playing={playing}
-      />
-    </div>
-  );
+  return <div className={styles.container}></div>;
 }
